e6d39135080ce29864779a3f097193dd676ff258
nikbatura <nikbatura@mail.ru>
2023-05-30 00:57:42 +0700

    Проверьте пожалуйста

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+    }
+
+    public static void task1() {
+        System.out.println("задача 1");
+        String a = "asdfgh";
+        int mid = a.length()/2;
+        String a1 = a.substring(0, mid);
+        String a2 = a.substring(mid);
+        System.out.println(a1);
+        System.out.println(a2);
+    }
+}
